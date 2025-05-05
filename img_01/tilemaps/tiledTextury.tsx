c288a97e36afacc756576d913fc082cdb67960ff
pozler_lukas <luki.pozler@gmail.com>
2025-05-05 08:37:55 +0200

    my files add

---
+++ img_01/tilemaps/tiledTextury.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="tiledTextury" tilewidth="50" tileheight="50" tilecount="500" columns="25">
+ <image source="png/tilemap_vykresleni.png" width="1250" height="1000"/>
+</tileset>
